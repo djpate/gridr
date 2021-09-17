7a0702b0248dd86258fcefd63bdd73c7d14f9b87
Christophe Verbinnen <christophev@knowbe4.com>
2021-09-17 18:03:57 -0400

    fixes

--- src/lib/grid.ts
+++ src/lib/grid.ts
@@ -1,5 +1,7 @@
 import { Coords, Placement } from "@/components/types"
 import { v4 as uuid } from 'uuid';
+import range from 'lodash/range'
+import intersection from 'lodash/intersection'
 
 // prevents the widget boundaries to leave the allowed grid
 export const constrainedInGrid = (coords: Coords, gridWidth: number): Coords => {
@@ -20,6 +22,13 @@ export const getCoordsForElement = (element: HTMLElement): Coords => {
   }
 }
 
+const placementRanges = (placement: Placement) : {col: number[], row: number[]} => {
+  return {
+    col: range(placement.col, placement.col + placement.width),
+    row: range(placement.row, placement.row + placement.height)
+  }
+}
+
 export class Widget {
   placement: Placement
   id: string
@@ -27,6 +36,18 @@ export class Widget {
   constructor(placement: Placement) {
     this.id = uuid()
     this.placement = placement
+  }
+
+  colides(anotherPlacement: Placement): boolean {
+    // if (this.id == anotherWidget.id) return false
+    const ranges = placementRanges(this.placement)
+    const otherRange = placementRanges(anotherPlacement)
+    // console.log('existing', this.columnRange)
+    // console.log('dragging', anotherWidget.columnRange)
+    // console.log('col intersection', intersection(this.columnRange, anotherWidget.columnRange))
+    // console.log('row intersection', intersection(this.rowRange, anotherWidget.rowRange))
+    return intersection(ranges.col, otherRange.col).length > 0 &&
+           intersection(ranges.row, otherRange.row).length > 0
   }
 }
 
@@ -55,12 +76,23 @@ export class Grid {
   }
 
   snappedPlacement(widget: Widget, coords: Coords): Placement {
-    return {
+    const desired: Placement = {
       col: Math.floor(coords.left / this.columnWidth),
       row: Math.floor(coords.top / this.rowHeight),
       width: Math.ceil(coords.width / (this.columnWidth + this.columnPadding)),
       height: Math.ceil(coords.height / (this.rowHeight + this.rowPadding))
     }
+    this.widgets.forEach((otherWidget) => {
+      if (widget.id !== otherWidget.id && otherWidget.colides(desired)) {
+        otherWidget.placement = {
+          col: otherWidget.placement.col,
+          row: (desired.row + desired.height),
+          width: otherWidget.placement.width,
+          height: otherWidget.placement.height
+        }
+      }
+    })
+    return desired
   }
 
   addWidget(widget: Widget) {
